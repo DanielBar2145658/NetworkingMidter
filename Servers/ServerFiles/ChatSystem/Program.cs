548c5999aec041de7ee14ef049e261b47cd5562c
Daniel Bar <98904765+DanielBar2145658@users.noreply.github.com>
2025-03-16 23:29:03 -0400

    ServerFiles

---
+++ Servers/ServerFiles/ChatSystem/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+public class TCPserver
+{
+
+    private static byte[] buffer = new byte[1024];
+    private static byte[] sendBuffer = new byte[1024];
+    private static Socket server;
+    private static string sendMsg = "";
+    private static List<Socket> clientSockets = new List<Socket>();
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Server...");
+        server = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+        ProtocolType.Tcp);
+        server.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888));
+        server.Listen(32);
+        server.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        Thread sendThread = new Thread(new ThreadStart(SendLoop));
+        sendThread.Start();
+        Console.ReadLine();
+    }
+    private static void AcceptCallback(IAsyncResult result)
+    {
+        Socket socket = server.EndAccept(result);
+        Console.WriteLine("Client connected!!");
+        clientSockets.Add(socket);
+        socket.BeginReceive(buffer, 0, buffer.Length, 0,
+        new AsyncCallback(ReceiveCallback), socket);
+        server.BeginAccept(new AsyncCallback(AcceptCallback), null);
+    }
+    private static void ReceiveCallback(IAsyncResult result)
+    {
+        Socket socket = (Socket)result.AsyncState;
+        int rec = socket.EndReceive(result);
+        string msg = Encoding.ASCII.GetString(buffer, 0, rec);
+        Console.WriteLine("Recv: " + msg);
+
+        sendMsg += " " + msg;
+        socket.BeginReceive(buffer, 0, buffer.Length, 0,
+        new AsyncCallback(ReceiveCallback), socket);
+    }
+    private static void SendCallback(IAsyncResult result)
+    {
+        Socket socket = (Socket)result.AsyncState;
+        socket.EndSend(result);
+    }
+    private static void SendLoop()
+    {
+        while (true)
+        {
+            //Protect this with a mutex
+            sendBuffer = Encoding.ASCII.GetBytes(sendMsg);
+            // Send updates to all clients in the list
+            foreach (var socket in clientSockets)
+            {
+                Console.WriteLine("Sent to: " +
+                socket.RemoteEndPoint.ToString());
+                socket.BeginSend(sendBuffer, 0, sendBuffer.Length, 0,
+                new AsyncCallback(SendCallback), socket);
+            }
+            sendMsg = "";
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Servers/ServerFiles/UDP/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class UDPserver
+{
+
+    public static void StartServer()
+    {
+        byte[] buffer = new byte[1024];
+
+        byte[] buffer2 = new byte[1024];
+
+//        string sendPosition = "";
+
+
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+        
+        IPEndPoint localEP = new IPEndPoint(ip, 8889);
+        //IPEndPoint localEPTCP = new IPEndPoint(ip, 8888);
+
+        Socket Server = new Socket(ip.AddressFamily,
+        SocketType.Dgram, ProtocolType.Udp);
+       
+        
+        IPEndPoint client1 = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint remoteClient = (EndPoint)client1;
+
+        IPEndPoint client2 = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint remoteClient2 = (EndPoint)client2;
+
+
+        try
+        {
+            Server.Bind(localEP);
+            Console.WriteLine("Waiting for data...");
+
+            //sendBuffer = Encoding.ASCII.GetBytes(sendPosition);
+
+            while (true)
+            {
+                int recv1 = Server.ReceiveFrom(buffer, ref remoteClient);
+
+                int recv2 = Server.ReceiveFrom(buffer2, ref remoteClient2);
+
+                Console.WriteLine("Received from: {0} ", remoteClient.ToString());
+                
+                Console.WriteLine("Data: {0}", Encoding.ASCII.GetString(buffer, 0,
+                recv1));
+
+                Console.WriteLine("Received from: {0} ", remoteClient2.ToString());
+
+                Console.WriteLine("Data: {0}", Encoding.ASCII.GetString(buffer2, 0,
+                recv2));
+
+                //Server.BeginSend(sendBuffer, 0, sendBuffer.Length, 0,
+                //    new AsyncCallback(SendCallback), recv1);
+
+                //Server.SendTo(buffer2, 0, buffer2.Length, 0, client1);
+                //Server.SendTo(buffer, 0, buffer.Length, 0, client2);
+
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    /*
+    private static void SendCallback(IAsyncResult result)
+    {
+        Socket socket = (Socket)result.AsyncState;
+        socket.EndSend(result);
+    }
+    */
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+}
